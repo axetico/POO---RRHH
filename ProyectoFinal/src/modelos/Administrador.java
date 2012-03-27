9d0c7a66de79c5705f48d1c97227bd0c702ca9df
Carlos Vera <carlosvera2004@hotmail.com>
2012-03-26 22:05:13 -0500

    mi segundo avance

--- ProyectoFinal/src/modelos/Administrador.java
+++ ProyectoFinal/src/modelos/Administrador.java
@@ -8,6 +8,11 @@ package modelos;
  *
  * @author CARLOS
  */
-public class Administrador {
+public class Administrador extends Usuario{
+
+    public Administrador(String nombre, String apellidoPaterno, String apellidoMaterno, String dni, String correoE, String fechaIngreso, String userName, String passWord, String confpassWord, String cargo, String rol) {
+        super(nombre, apellidoPaterno, apellidoMaterno, dni, correoE, fechaIngreso, userName, passWord, confpassWord, cargo, rol);
+    }
+
     
 }
--- ProyectoFinal/src/modelos/Empleado.java
+++ ProyectoFinal/src/modelos/Empleado.java
@@ -2,22 +2,11 @@
 package modelos;
 
 
+
 public class Empleado extends Usuario{
-    public String codEmpleado;
 
-    public Empleado(String codEmpleado, String nombre, String apellido, String userName, String passWord) {
-        super(nombre, apellido, userName, passWord);
-        this.codEmpleado = codEmpleado;
+    public Empleado(String nombre, String apellidoPaterno, String apellidoMaterno, String dni, String correoE, String fechaIngreso, String userName, String passWord, String confpassWord, String cargo, String rol) {
+        super(nombre, apellidoPaterno, apellidoMaterno, dni, correoE, fechaIngreso, userName, passWord, confpassWord, cargo, rol);
     }
-
-    public String getCodEmpleado() {
-        return codEmpleado;
-    }
-
-    public void setCodEmpleado(String codEmpleado) {
-        this.codEmpleado = codEmpleado;
-    }
-    
-    
     
 }
